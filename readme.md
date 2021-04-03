6c068ee0051e0966449bb31f68f4ba173173bc15
Mushroom King <xiayingp@gmail.com>
2021-04-03 00:21:14 -0700

    done

---
+++ readme.md
@@ -0,0 +1,18 @@
+Read V2 PDFs, parse the keywords, append to new CSV file.
+
+For HUAXIA.
+
+## Install
+```
+pip install virtualenv
+
+virtualenv myproject source myproject/venv/bin/activate
+
+pip -r requirements.txt
+```
+
+### how to dump dep
+```
+pip3 install pipreqs
+5680  pipreqs . --force
+```
--- run.py
+++ run.py
@@ -28,36 +28,56 @@ def parse_report(text):
 
     lines = text.splitlines()
     date_and_invoice = lines[2].split()
-    d[0] = date_and_invoice[0]
-    d[1] = date_and_invoice[1]
+    d[1] = date_and_invoice[0]
+    d[2] = date_and_invoice[1]
     
     # AWB number
     awb_line = lines[4]
-    d[2] = awb_line.split()[-1]
+    d[3] = awb_line.split()[-1]
     
     # Destination
-    dest_line = lines[15].split()
-    d[3] = ' '.join(dest_line[3:-1])
+    dest_line = ""
     
-    # 5 6 7 10 11
-    desc = lines[19].split()
-    d[10] = desc[2]
-    d[11] = desc[6]
-    d[5] = desc[7]
-    d[6] = desc[8]
-    d[7] = desc[-1]
+    desc = ""
+    x_ray_line = ""
+    hawb_line = ""
+    for i, l in enumerate(lines):
+        if 'X-RAY' in l:
+            x_ray_line = l.split()
+        elif 'HAWB' in l:
+            hawb_line = l.split()
+        elif 'Transport' in l:
+            desc = l.split()
+        elif 'Origin Destination Currency' in l:
+            dest_line = lines[i+1].split()
     
+    if desc:
+        # 5 6 7 10 11
+        d[10] = desc[2]
+        d[11] = desc[6]
+        d[5] = desc[7]
+        d[6] = desc[8]
+        d[7] = desc[-1]
+    
+    if dest_line:
+        d[4] = ' '.join(dest_line[-3:-1])
+
+    
+    if x_ray_line:
+        d[8] = x_ray_line[-1]
+    
+    if hawb_line:
+        d[9] = hawb_line[-1]
     
     # total
     total_line = lines[-1].split()
     d[12] = total_line[1]
     
-    
     return d
     
 def run():
-    text = read_pdf_as_text('input/test1.pdf')
-    #print(text)
+    text = read_pdf_as_text('input/invoice_210203V5802.pdf')
+    print(text)
     report = parse_report(text)
     print(report)
 
@@ -67,7 +87,7 @@ def eval_folder(folder='input'):
     for f in onlyfiles:
         print('Processing..', f)
         report = parse_report(read_pdf_as_text(f))
-        #print(report)
+        print(report)
         reports.append(report)
     
     return reports
@@ -76,20 +96,23 @@ def csv_writer(reports):
     with open('output/result.csv', "w") as csv_file:
         writer = csv.writer(csv_file, delimiter=',')
         # write headers
-        header = ['日期','Invoice No.','总运单号','清关公司','Chargable Weight','Freight','单价','麻袋数',
-            '毛重','泡重产生的额外费用','提单重量','重量差','提单号']
+        header = ['日期','Invoice No.','总运单号','清关公司','Chargable Weight','单价',
+             'Freight', 'X-RAY', '额外费用', '麻袋数', '毛重', '费用']
         writer.writerow(header)
         
         # contents
         for report in reports:
             l = []
-            for i in range(1, 10):
-                l.append(report[i])
+            for i in range(1, 13):
+                if i in report:
+                    l.append(report[i])
+                else:
+                    l.append('N/A')
                 
             # writerow requires a list
             writer.writerow(l)
     
-#reports = eval_folder()
-#csv_writer(reports)
+reports = eval_folder()
+csv_writer(reports)
 
-run()
+#run()
